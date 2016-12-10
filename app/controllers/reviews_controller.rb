ff7866e483dd68ba6ebccc7f4120a3b1e4dc965d
jspuravida <julian.salari@gmail.com>
2016-12-10 05:53:31 +0000

    Added review saving

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,35 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    @review.user = current_user
+
+
+    if ! @review.valid?
+      puts "BAD REVIEW"
+      puts @review.errors.messages
+    end
+
+
+
+# person = Person.new
+# person.valid? # => false
+# person.errors.messages
+
+
+    if @review.save
+      puts "I saved"
+      redirect_to product_path(@product)
+    else
+      puts "save failed"
+      redirect_to '/'
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:description, :rating)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
 
-  belongs_to :product
+  belongs_to :product, required: true
   belongs_to :user
 
   validates :product_id, presence: true
